441cdc804a2e57686b628a854e83de9447d0521c
Valeriia Kyrychenko <valeriia.kyrychenko22@gmail.com>
2022-12-17 22:48:32 +0200

    добавляет библиотеку  simpleLightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,8 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+console.log(SimpleLightbox);
+console.log("hello kitty");
