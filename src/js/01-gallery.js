63ddcb9403b2cebc972b09dcf6cd190875b7865c
Valeriia Kyrychenko <valeriia.kyrychenko22@gmail.com>
2022-12-17 23:08:43 +0200

    добавляет решение задачи №1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -7,4 +7,19 @@ console.log(galleryItems);
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 console.log(SimpleLightbox);
-console.log("hello kitty");
+
+const list = document.querySelector(".gallery");
+
+
+const markup = galleryItems.reduce((acc, { preview,original,description }) => acc + `<a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>`, "")
+
+
+list.insertAdjacentHTML("beforeend", markup);
+
+const lightbox = new SimpleLightbox('.gallery a', {captionsData: "alt", captionDelay: 250});
