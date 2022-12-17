976e9ba4d3016915bdf7984fffff59866bca9b69
Valeriia Kyrychenko <valeriia.kyrychenko22@gmail.com>
2022-12-18 00:55:23 +0200

    добавляет черновик решения задачи №2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,5 +1,33 @@
- 
+ import Player from '@vimeo/player';
 
-const iframe = document.querySelector("#vimeo-player");
-const player = new Vimeo.Player(iframe);
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
 console.log(player);
+
+
+const handlePlay = function(time) {localStorage.setItem("videoplayer-current-time", JSON.stringify(time));
+
+    const current = localStorage.getItem("videoplayer-current-time");
+    const parsedCurrent = JSON.parse(current);
+    return parsedCurrent.seconds;
+    
+};
+
+
+
+player.on('timeupdate', handlePlay);
+
+player.setCurrentTime(handlePlay).then(function(handlePlay) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
