aa92f528c8e2c93b68df670d804defea3f1feb47
valerykyr23 <95191772+valerykyr23@users.noreply.github.com>
2022-12-17 21:52:13 +0000

    Deploying to gh-pages from @ valerykyr23/goit-js-hw-08@bac9820f256c6763206fb9d17f034236aa3a45ac 🚀

---
+++ 02-video.8ea1ee96.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("#vimeo-player"),o=new Vimeo.Player(e);console.log(o)}();
+//# sourceMappingURL=02-video.8ea1ee96.js.map
---
+++ 02-video.e17eb240.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("#vimeo-player"),o=new Vimeo.Player(e);console.log(o);
+//# sourceMappingURL=02-video.e17eb240.js.map
