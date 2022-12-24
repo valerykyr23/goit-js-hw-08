3fefcd590451dee46f468debef44e8df720406db
Valeriia Kyrychenko <valeriia.kyrychenko22@gmail.com>
2022-12-24 21:14:54 +0200

    вносит поправки в задачу №2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,7 +1,8 @@
 import Player from '@vimeo/player';
  import _throttle from 'lodash.throttle';
 
- const iframe = document.querySelector('#vimeo-player');
+ 
+const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 
 console.log(player);
@@ -9,20 +10,19 @@ console.log(player);
 
 function handlePlay(time) {
  
-    
     localStorage.setItem("videoplayer-current-time",JSON.stringify(time.seconds))  
 }
 
 function settingCurentTime() {
-    const sometime = localStorage.getItem("videoplayer-current-time");
+    const sometime = localStorage.getItem("videoplayer-current-time") || 0;
     
     player.setCurrentTime(sometime);
 
 }
     
-    
-
 player.on('timeupdate', _throttle(handlePlay,1000));
 
 player.on("loaded", settingCurentTime);
+
+
 
